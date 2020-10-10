3cf8ec81ba55094437d2656ad2ee3c058eee4fe6
Daniel Doña <daniel.dona@xnor.ga>
2020-10-10 00:19:01 +0000

    Modify

--- 00_create_db.sh
+++ 00_create_db.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Setting up default IDS PostgreSQL user!\n"
+echo "Setting up default IDS PostgreSQL user!"
 
 set -e
 
 psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER $DATABASE_USER WITH PASSWORD '$DATABASE_USER_PW' ;
+	CREATE USER $DATABASE_USER PASSWORD '$DATABASE_USER_PW' ;
 	CREATE DATABASE ids OWNER $DATABASE_USER;
 	EOSQL
